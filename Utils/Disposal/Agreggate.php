2143b59339fc159d10ab6d4fb8b47abb51db9870
Mark Ogilvie <mogilvie@users.noreply.github.com>
2018-01-30 14:38:36 +0000

    Create Agreggate.php

---
+++ Utils/Disposal/Agreggate.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * GdprBundle/Utils/Disposal/Aggregate.php.
+ *
+ * @author      Mark Ogilvie <mark.ogilvie@specshaper.com>
+ * @copyright   (c) 2018, SpecShaper - All rights reserved
+ *
+ */
+namespace GdprBundle/Utils/Disposal;
+
+use GdprBundle/Utils/Disposal/DisposalInterface;
+
+/**
+ * Aggregate
+ *
+ * Aggregate a value to a range.
+ *
+ * @author      Mark Ogilvie <mark.ogilvie@specshaper.com>
+ * @license     http://URL name
+ *
+ * @version     Release: 1.0.0
+ */
+class Aggregate implements DisposalInterface
+{
+ 
+    public function dispose($parameter){
+       return null;
+    }
+ 
+}
