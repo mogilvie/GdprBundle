af8a074d6cc2586d1fed2f5dba2591742815d89a
Mark Ogilvie <mogilvie@users.noreply.github.com>
2018-01-30 14:37:21 +0000

    Create Categorise.php

---
+++ Utils/Disposal/Categorise.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * GdprBundle/Utils/Disposal/Categorise.php
+ *
+ * @author      Mark Ogilvie <mark.ogilvie@specshaper.com>
+ * @copyright   (c) 2018, SpecShaper - All rights reserved
+ *
+ */
+namespace GdprBundle/Utils/Disposal;
+
+use GdprBundle/Utils/Disposal/DisposalInterface;
+
+/**
+ * Categorise
+ *
+ * Change a value to a category for anonymisation.
+ *
+ * @author      Mark Ogilvie <mark.ogilvie@specshaper.com>
+ * @license     http://URL name
+ *
+ * @version     Release: 1.0.0
+ */
+class Categorise implements DisposalInterface
+{
+    public function dispose($parameter){
+       return null;
+    }
+}
