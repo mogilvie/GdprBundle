fd7785eae96ccf3960f60d5266d386c3087bf420
Mark Ogilvie <mogilvie@users.noreply.github.com>
2018-01-30 14:52:56 +0000

    Spelling correction

--- Utils/Disposal/AggregateDate.php
+++ Utils/Disposal/AggregateDate.php
@@ -29,8 +29,8 @@ class AggregateDate implements DisposalInterface
     const AGGREGATE_BY_YEAR = 'YEAR';
     const AGGREGATE_BY_HALF_DECADE = 'HALF_DECADE';
     const AGGREGATE_BY_DECADE = 'DECADE';
-    const AGGREGATE_BY_HALF_CENTURAY = 'HALF_CENTURARY';
-    const AGGREGATE_BY_CENTURARY = 'CENTURARY';
+    const AGGREGATE_BY_HALF_CENTURY = 'HALF_CENTURY';
+    const AGGREGATE_BY_CENTURY = 'CENTURY';
  
     private $aggregateBy;
  
@@ -58,9 +58,9 @@ class AggregateDate implements DisposalInterface
                 break;
             case self::AGGREGATE_BY_DECADE:
                 break;
-            case self::AGGREGATE_BY_HALF_CENTURAY:
+            case self::AGGREGATE_BY_HALF_CENTURY:
                 break;
-            case self::AGGREGATE_BY_CENTURARY:
+            case self::AGGREGATE_BY_CENTURY:
                 break;
         }
     }
