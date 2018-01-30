06b3dd716d8ab0c3eef2c689b143ad1b05c03e24
Mark Ogilvie <mogilvie@users.noreply.github.com>
2018-01-30 14:15:24 +0000

    Update AggregateDate.php

--- Utils/Disposal/AggregateDate.php
+++ Utils/Disposal/AggregateDate.php
@@ -6,72 +6,40 @@
  * @copyright   (c) 2018, SpecShaper - All rights reserved
  *
  */
- 
 namespace GdprBundle/Utils/Disposal/AggregateDate;
 
+use GdprBundle/Utils/Disposal/AggregateInterface;
+
 /**
- * EncryptEvent.
+ * AggregateDate.
+ *
+ * A class to convert a date to an aggregated value;
  *
  * @author      Mark Ogilvie <mark.ogilvie@specshaper.com>
- * @copyright   (c) 2015, SpecShaper - All rights reserved
  * @license     http://URL name
  *
  * @version     Release: 1.0.0
- *
- * @since       Available since Release 1.0.0
  */
-class AggregateDate
+class AggregateDate implements AggregateInterface
 {
-    /**
-     * The string / object to be encrypted or decrypted
-     *
-     * @since Available since Release 1.0.0
-     *
-     * @var string
-     */
-    protected $value;
-    
-    /**
-     * @var string
-     */
-    protected $action;
-    /**
-     * EncryptEvent constructor.
-     *
-     * @param        $value
-     * @param string $action
-     */
-    public function __construct($value, $action = EncryptEvents::ENCRYPT)
-    {
-        $this->value= $value;
-        $this->action = $action;
+    const AGGREGATE_BY_DAY = 'DAY';
+    const AGGREGATE_BY_WEEK = 'WEEK';
+    const AGGREGATE_BY_MONTH = 'MONTH';
+    const AGGREGATE_BY_QUARTER = 'QUARTER';
+    const AGGREGATE_BY_YEAR = 'YEAR';
+    const AGGREGATE_BY_HALF_DECADE = 'HALF_DECADE';
+    const AGGREGATE_BY_DECADE = 'DECADE';
+    const AGGREGATE_BY_HALF_CENTURAY = 'HALF_CENTURARY';
+    const AGGREGATE_BY_CENTURARY = 'CENTURARY';
+ 
+    private $aggregateBy;
+ 
+    public function __construct($aggregateBy){
+       $this->aggregateBy = $aggregateBy;
     }
-    /**
-     * @return string
-     */
-    public function getValue(): string
-    {
-        return $this->value;
+ 
+    public function aggregate(\DateTimeInterface $dateTime){
+       return;
     }
-    /**
-     * @param string $value
-     */
-    public function setValue(string $value)
-    {
-        $this->value = $value;
-    }
-    /**
-     * @return string
-     */
-    public function getAction(): string
-    {
-        return $this->action;
-    }
-    /**
-     * @param string $action
-     */
-    public function setAction(string $action)
-    {
-        $this->action = $action;
-    }
+ 
 }
