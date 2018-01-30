89c9bc78f1c49e06a054480f08b82d7acbcf1a94
Mark Ogilvie <mogilvie@users.noreply.github.com>
2018-01-30 15:15:17 +0000

    Create ExcelExportService.php

---
+++ Utils/Portability/ExcelExportService.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * SpecShaper/GdprBundle/Utils/Portability/ExportService.php.
+ *
+ * @author      Mark Ogilvie <mark.ogilvie@specshaper.com>
+ */
+namespace SpecShaper/GdprBundle/Utils/Portability;
+
+use SpecShaper/GdprBundle/Utils/Portability/ExportServiceInterface;
+
+/**
+ * ExportService.
+ *
+ * A service to export database information in Excel.
+ *
+ * @author      Mark Ogilvie <mark.ogilvie@specshaper.com>
+ * @license     http://URL name
+ *
+ * @version     Release: 1.0.0
+ */
+class ExcelExportService implements ExportServiceInterface
+{
+    
+    public function export(){
+    
+    }
+}
