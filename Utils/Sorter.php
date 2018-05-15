1b7d5b7e332d6e805d4cb8c49c39a768cbb7c8be
Mark Ogilvie <mark.ogilvie@ogilvieconsulting.net>
2018-05-16 00:06:29 +0100

    Added a sorter for post hydration sorting. Added notes to readme.

--- Utils/Sorter.php
+++ Utils/Sorter.php
@@ -4,6 +4,9 @@ namespace SpecShaper\GdprBundle\Utils;
 
 class Sorter
 {
+
+    const ORDER_DESC = 'DESC';
+    const ORDER_ASC = 'ASC';
     public $firstSortOrder;
 
     protected $secondSortOrder;
@@ -17,22 +20,36 @@ class Sorter
     /**
      * Order an array by two fields,
      *
-     * @param $a
-     * @param $b
+     * @param        $a
+     * @param        $b
+     * @param string $order
+     *
      * @return int
      */
-    public function sortByTwoColumnsCallback($a, $b){
+    public function sortByTwoColumnsCallback($a, $b, $order = self::ORDER_DESC){
 
         $firstOrder = $this->firstSortOrder;
 
         $secondOrder= $this->secondSortOrder;
+
+        $methodArray = array(self::ORDER_DESC => 'descSort', self::ORDER_ASC => 'ascSort');
 
         if ($a[$firstOrder] == $b[$firstOrder])
         {
             // employeeId is the same, sort by lastName
             if ($a[$secondOrder] > $b[$secondOrder]) return 1;
         }
-        // sort the higher employeeId first:
-        return $a[$firstOrder] < $b[$firstOrder] ? 1 : -1;
+
+        if($method = $methodArray[$order]) {
+            return $this->$method($a[$firstOrder],$b[$firstOrder]);
+        }
+    }
+
+    private function descSort($aField, $bField){
+        return $aField < $bField ? 1 : -1;
+    }
+
+    private function ascSort($aField, $bField){
+        return $aField > $bField ? 1 : -1;
     }
 }
