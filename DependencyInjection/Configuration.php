fc675f61ab91af0d09b7efd2ec9d901bc993b081
Mark Ogilvie <mogilvie@users.noreply.github.com>
2020-04-27 00:50:17 +0100

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder('spec_shaper_gdpr');
-
+//        $rootNode = $treeBuilder->getRootNode();
 //            $rootNode
 //                ->children()
 //                    ->booleanNode('encrypt_personal_data')->defaultValue(false)->end()
