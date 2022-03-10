196a0407e5e2b306124ac871f13fa47680a4e5b6
Mark Ogilvie <mogilvie@users.noreply.github.com>
2022-03-10 09:44:56 +0000

    Add typed return to clear deprecation

--- Form/Type/PersonalDataType.php
+++ Form/Type/PersonalDataType.php
@@ -33,7 +33,7 @@ class PersonalDataType extends AbstractType
         ));
     }
 
-    public function getParent()
+    public function getParent(): ?string
     {
         return TextType::class;
     }
